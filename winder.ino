3ef8e62f240df6f043fe3a97eabec024a2f2372d
Oops <questionable.mechanics@gmail.com>
2021-03-01 09:19:09 +0100

    Update winder.ino

--- winder.ino
+++ winder.ino
@@ -2,7 +2,7 @@
 SIMPLE WATCH WINDER
   by: Questionable Mechanics
   contact: questionable.mechanics@gmail.com
-  last rev: 02/2021
+  last rev: 03/2021
   This sketch is in the public domain
   
   Notes: 
@@ -103,7 +103,7 @@ void winderCW() {
 */
 void loop() {
   if (limit == false) {
-    winderCW();   //limOptions = alternate winder rotate on long button press; pause on short press
+    winderCW(); 
   }
   else if (limit == true) {
     winderCCW();
@@ -111,36 +111,76 @@ void loop() {
 }
 
 /*
+ * 
+ * FOR EVENTUAL TACTILE SWITCH REPLACEMENT
+ * 
+unsigned long keyPrevMillis = 0;
+const unsigned long keySampleIntervalMs = 25;
+byte longKeyPressCountMax = 80;    // 80 * 25 = 2000 ms
+byte longKeyPressCount = 0;
 
-NOTES FOR limOptions
-====================
+byte prevKeyState = HIGH;         // button is active low
+const byte keyPin = 18;            // button is connected to pin 2 and GND
 
-const int SHORT_PRESS_TIME = 1000; 
-const int LONG_PRESS_TIME  = 5000;
 
-int lastState = LOW;  
-int currentState;     
-unsigned long pressedTime  = 0;
-unsigned long releasedTime = 0;
-
-currentState = digitalRead(LIM_PIN);
-
-if (lastState == HIGH && currentState == LOW)        // button is pressed
-    pressedTime = millis();
-  else if(lastState == LOW && currentState == HIGH) { // button is released
-    releasedTime = millis();
-
-    long pressDuration = releasedTime - pressedTime;
-
-    if (pressDuration < SHORT_PRESS_TIME )
-      //SWITCH TO CLOCKWISE
-
-    if (pressDuration > LONG_PRESS_TIME )
-      //PAUSE FOR 30 SECONDS
-  }
-
-  // save the the last state
-  lastState = currentState;
+// called when button is kept pressed for less than 2 seconds
+void shortKeyPress() {
+    Serial.println("short");
 }
 
-*/
+
+// called when button is kept pressed for more than 2 seconds
+void longKeyPress() {
+    Serial.println("long");
+}
+
+
+// called when key goes from not pressed to pressed
+void keyPress() {
+    Serial.println("key press");
+    longKeyPressCount = 0;
+}
+
+
+// called when key goes from pressed to not pressed
+void keyRelease() {
+    Serial.println("key release");
+   
+    if (longKeyPressCount >= longKeyPressCountMax) {
+        longKeyPress();
+    }
+    else {
+        shortKeyPress();
+    }
+}
+
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(keyPin, INPUT_PULLUP);
+}
+
+
+void loop() {
+    // key management section
+    if (millis() - keyPrevMillis >= keySampleIntervalMs) {
+        keyPrevMillis = millis();
+       
+        byte currKeyState = digitalRead(keyPin);
+       
+        if ((prevKeyState == HIGH) && (currKeyState == LOW)) {
+            keyPress();
+        }
+        else if ((prevKeyState == LOW) && (currKeyState == HIGH)) {
+            keyRelease();
+        }
+        else if (currKeyState == LOW) {
+            longKeyPressCount++;
+        }
+       
+        prevKeyState = currKeyState;
+    }
+
+
+    // other code goes here
+}*/
